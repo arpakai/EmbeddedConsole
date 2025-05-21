d3188dac8cb93cf4e4a206b95404a98699fc81de
Arda Hoysaloglu <cemalhoysaloglu@gmail.com>
2025-05-21 22:25:37 +0300

    .cmake file added

---
+++ EmbeddedConsole.cmake
@@ -0,0 +1,14 @@
+# INCLUDE GUARD
+include_guard()
+
+message("EmbeddedConsole lib is included")
+
+target_sources(${PROJECT_NAME} PRIVATE
+
+)
+
+target_include_directories(${PROJECT_NAME} PRIVATE
+
+)
+
+
